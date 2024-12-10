79e03c40c85423e1603e9c9c20284fda0d793567
manalizo <yumimanal8@gmail.com>
2024-12-10 21:09:17 +0100

    firstore service

--- lib/controllers/FirestoreService.dart
+++ lib/controllers/FirestoreService.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:salin/models/shopping_item.dart';
+import 'package:firebase_auth/firebase_auth.dart'; // For getting the current user
 
 class FirestoreService {
   final FirebaseFirestore _db = FirebaseFirestore.instance;
@@ -29,12 +30,21 @@ class FirestoreService {
         .update({'isBought': isBought});
   }
 
-  // Add a new shopping item
   Future<void> addShoppingItem(String listName, ShoppingItem item) async {
-    await _db
-        .collection('shopping_lists')
+    String userId = FirebaseAuth.instance.currentUser!.uid; // Get current user ID
+
+    // Add the shopping item to Firestore with the userId in shopping_lists
+    await _db.collection('shopping_lists')
         .doc(listName)
         .collection('items')
-        .add(item.toMap());
+        .add({
+      ...item.toMap(),
+      'userId': userId, // Add userId to each item
+    });
+
+    // Optionally, you can add userId to the shopping list itself (not individual items)
+    await _db.collection('shopping_lists').doc(listName).set({
+      'userId': userId, // Add userId to the shopping list document
+    }, SetOptions(merge: true));
   }
 }
